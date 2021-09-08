e497f4617204918583c4d4f1d4df39c05a68e2d9
Bedkow <bednarekdaniel@o2.pl>
2021-09-08 22:00:13 +0200

    basic setup done

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import _ from "lodash";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
+
+function component() {
+	return element;
+}
+
+document.querySelector("output-below-here").appendChild(component());
